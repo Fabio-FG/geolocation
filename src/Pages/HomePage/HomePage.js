4dc46beda3e00c8d5caacf89f249a925f7afb515
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-01-21 00:41:06 +0000

    add media queries

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -4,20 +4,26 @@ import Map from "../../Components/Map/Map";
 function HomePage() {
   return (
     <div>
-    <div className="second-header">
-      <section className="text-container">
-      <h1 className="title">Pedal with World Bike</h1>
+      <div className="second-header">
+        <section className="text-container">
+          <h1 className="title">Pedal with World Bike</h1>
 
-        <p className="text">
-          Search for Cycling networks and stations all over the world in a few
-          clicks.
-        </p>
-      </section>
-     </div>
+          <p className="text">
+            Search for Cycling networks and stations all over the world in a few
+            clicks.
+          </p>
+        </section>
+      </div>
       <Map />
       <section id="about">
-        <p>This World Bike app was built using React and Leaflet JS a tool that allows us to render an openstreet map. Leaflet JS uses materials such as Markers and Clusters.<br>
-        </br>The networks are displayed worldwide and cluster together according to the zoom level.</p>
+      <h3>About World Bike</h3>
+        <p>
+          This World Bike app was built using React and Leaflet JS a tool that
+          allows us to render an openstreet map. Leaflet JS uses materials such
+          as Markers and Clusters. The networks are displayed worldwide and
+          cluster together according to the zoom level.
+          For more information read the documentation at <a href="https://react-leaflet.js.org/docs/start-introduction/" alt="react-leaflet-doc" className="doc-link">Leaflet-React</a>.
+        </p>
       </section>
     </div>
   );
