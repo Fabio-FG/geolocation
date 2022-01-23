3dadd4459e67b1ee74ee28b94c6a8500ca1823d6
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-01-23 01:18:44 +0000

    contact form styling

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -1,16 +1,26 @@
 import "./ContactPage.css";
+import githubLogo from "../../images/github.svg";
+import linkedinLogo from "../../images/linkedin.svg";
+import gmailLogo from "../../images/gmail.svg";
 
 function ContactPage() {
   return (
     <div>
-      <form>
-        <h1>Contact Us</h1>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Your Message" maxlength="350"/>
-        {/* Add a reCAPTCHA */}
-        <button type="submit">Submit</button>
-      </form>
+      <div className="contact-wrapper">
+        <div className="contact-links">
+          <img src={githubLogo} alt="github-logo" id="github-logo" className="icon"/>
+          <img src={linkedinLogo} alt="linkedin-logo" id="linkedin-logo" className="icon"/>
+          <img src={gmailLogo} alt="gmail-logo" id="gmail-logo" className="icon"/>
+        </div>
+        <form>
+          <h1>Contact Us</h1>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Your Message" maxlength="350" />
+          {/* Add a reCAPTCHA */}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
