e774174a06c3c125f7defa90287fddcd2b81c164
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-02-02 01:02:23 +0000

    add contact page translations

--- src/Pages/ContactPage/ContactPage.js
+++ src/Pages/ContactPage/ContactPage.js
@@ -2,8 +2,13 @@ import "./ContactPage.css";
 import githubLogo from "../../images/github.svg";
 import linkedinLogo from "../../images/linkedin.svg";
 import gmailLogo from "../../images/gmail.svg";
+import { useTranslation } from "react-i18next";
 
 function ContactPage() {
+
+  //translation hook
+  const { t } = useTranslation();
+
   return (
       <div>
     <div className="content-wrapper">
@@ -14,12 +19,12 @@ function ContactPage() {
           <img src={gmailLogo} alt="gmail-logo" id="gmail-logo" className="icon"/>
         </div>
         <form>
-          <h1>Contact Us</h1>
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <textarea placeholder="Your Message" maxlength="350" />
+          <h1>{t("Contact Us")}</h1>
+          <input type="text" placeholder={t("Name")} />
+          <input type="email" placeholder={("Email")} />
+          <textarea placeholder={t("Your Message")} maxlength="350" />
           {/* Add a reCAPTCHA */}
-          <button type="submit" className="submit-btn">Submit</button>
+          <button type="submit" className="submit-btn">{t("Submit")}</button>
         </form>
         </div>
         </div>
