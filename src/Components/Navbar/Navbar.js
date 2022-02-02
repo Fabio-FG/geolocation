be19e64ac10af6536d01710c479b0ae5162eb60f
fabioFG <fabio.fernando.guerreiro@gmail.com>
2022-02-02 00:05:33 +0000

    change language display

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,20 +2,19 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
+/* import { useState } from "react"; */
 
+function Navbar({ languages }) {
+  //refreshing the page when clicking on the home button in the homepage.
+  const refreshPage = () => {
+    window.scrollTo(0, 0);
+  };
 
-function Navbar({languages}) {
+  const { t } = useTranslation();
 
- 
-//refreshing the page when clicking on the home button in the homepage.
-const refreshPage = () => {
-  window.scrollTo(0, 0);
-};
-
-
-const { t } = useTranslation();
-
-
+  //useState to display languages
+ /*  const [showLanguages, setShowLanguages] = useState(true); */
+  //dropdown function
 
   return (
     <div className="container">
@@ -23,32 +22,43 @@ const { t } = useTranslation();
       <header className="header">
         <input type="checkbox" className="nav-toggle" id="nav-toggle" />
         <label for="nav-toggle" className="nav-toggle-label">
-        {/* Hamburguer using span */}
+          {/* Hamburguer using span */}
           <span></span>
         </label>
-        
+
         <div className="logo">
-        <h1><Link to="/" className="my-name" onClick={refreshPage}>WorldBike 🚲</Link></h1>
+          <h1>
+            <Link to="/" className="my-name" onClick={refreshPage}>
+              WorldBike 🚲
+            </Link>
+          </h1>
         </div>
 
         <nav className="nav-container">
-       
           <a href="#about">{t("About")}</a>
           <Link to="/contacts">{t("Contacts")}</Link>
 
-          {languages.map(({ code, name, codename, country_code }) => {
-              return (
-                <li className="langs">
-                  <button
-                    onClick={() => {
-                      i18next.changeLanguage(code);
-                    }}
-                  >
-                    {codename}
-                  </button>
-                </li>
-              );
-            })}
+          <div className="language-wrapper">
+            {
+              languages.map(({ code, name, codename, country_code }) => {
+                return (
+                  
+                  <ul className="dropdown-content">
+                    <li className="langs">
+                      <button
+                        onClick={() => {
+                          i18next.changeLanguage(code);
+                        }}
+                        className="language-btn"
+                      >
+                        {codename}
+                      </button>
+                    </li>
+                  </ul>
+                 
+                );
+              })}
+          </div>
         </nav>
       </header>
     </div>
